347cfe264c6ab26f45081e18136f07fc255f3291
AlexBaily <alex.d.baily@gmail.com>
2020-04-20 18:32:34 +0100

    Add rootHandler unit test.

--- api/rootHandler.go
+++ api/rootHandler.go
@@ -2,9 +2,6 @@ package api
 
 import (
 	"net/http"
-	"github.com/dgrijalva/jwt-go"
-	"fmt"
-
 )
 
 //Http handler for responding to http/s requests.
@@ -14,9 +11,5 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("statusDescription", "200 OK")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	user := r.Context().Value("user");
-	for k, v := range user.(*jwt.Token).Claims.(jwt.MapClaims) {
-		fmt.Fprintf(w, "%s :\t%#v\n", k, v)
-	  }
 	w.Write([]byte("{\"Route\":\"Test\"}"))
 }
