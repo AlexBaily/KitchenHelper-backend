ec0d36dae8f6ab263e11d4635730333a3ca3b7ea
AlexBaily <alex.d.baily@gmail.com>
2020-11-17 22:52:08 +0000

    Add test for QueryRecipes

--- api/db_test.go
+++ api/db_test.go
@@ -90,3 +90,39 @@ func TestAddLocation(t *testing.T) {
 	}
 
 }
+
+func TestQueryRecipes(t *testing.T) {
+	cases := []struct {
+		Resp     *dynamodb.QueryOutput
+		Expected []byte
+	}{
+		{
+			Resp: &dynamodb.QueryOutput{
+				Items: []map[string]*dynamodb.AttributeValue{
+					{
+						"RecipeIdentifier": &dynamodb.AttributeValue{
+							S: aws.String("bean burritos"),
+						},
+					},
+				},
+			},
+			Expected: []byte("{\"recipes\":[\"bean burritos\"]}"),
+		},
+	}
+
+	for i, c := range cases {
+		db := DynamoInt{
+			Client: mockedDynamo{Resp: c.Resp},
+		}
+		rows := db.queryRecipes("testuserid", "recipestable")
+		if a, e := rows, c.Expected; bytes.Compare(a, e) != 0 {
+			t.Fatalf("%d, expected %d row, got %d", i, e, a)
+		}
+		for j, rows := range rows {
+			if a, e := rows, c.Expected[j]; a != e {
+				t.Errorf("%d, expected %v row, got %v", i, e, a)
+			}
+		}
+	}
+
+}
