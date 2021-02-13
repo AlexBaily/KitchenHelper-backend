b0b4a9f813c497313bfe0f0518eb45de3bb1d98e
AlexBaily <alex.d.baily@gmail.com>
2021-02-13 09:33:46 +0000

    Add JSON attributes to models.

--- models/location.go
+++ models/location.go
@@ -2,13 +2,13 @@ package models
 
 //Record struct that will house the DynamoDB records.
 type LocRecord struct {
-	UserID       string 
-	ProductIdentifier	 string  
-	Location	string `json:"location"`
+	UserID            string `json:UserID`
+	ProductIdentifier string `json:productIdentifier`
+	Location          string `json:"location"`
 }
 
 type ProductRecord struct {
-	ProductIdentifier string
-	ProductName string
-	Quantity       int
+	ProductIdentifier string `json:productIdentifier`
+	ProductName       string `json:productName`
+	Quantity          int    `json:quantity`
 }
